2bc9d2b9f3d57f3324901ee50ecc6e434d45391f
chris-jt <chtronconi@gmail.com>
2024-08-07 15:26:15 -0300

    primer commit

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3"{
+    bucket                 = "mundose-tf-2024"
+    region                 = "us-east-1"
+    key                    = "backend.tfstate"
+    #dynamodb_table         = "terraformstatelock"
+  }
+}
+
---
+++ setup2.tf
@@ -0,0 +1,46 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_vpc" "vpc_default" {
+  default = true
+} 
+
+#data source para obtener la region
+data "aws_region" "current" {}
+
+#data source para id de la cuenta de AWS
+data "aws_caller_identity" "current" {}
+
+
+#Create SG for allowing TCP/80 & TCP/22
+resource "aws_security_group" "sg" {
+  name        = "webserver-sg"
+  description = "Allow TCP/80 & TCP/22"
+  vpc_id      = data.aws_vpc.vpc_default.id
+  ingress {
+    description = "Allow SSH traffic"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "allow traffic from TCP/80"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+output "Webserver-Public-IP" {
+  value = aws_instance.webserver.public_ip
+}
